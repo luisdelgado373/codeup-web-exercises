d414e3c393a2c7b7dbe95df6e9006dec673052fd
Luis Delgado <delgadoluis0510@gmail.com>
2019-08-07 15:30:01 -0500

    adding conditionals lec

---
+++ js/conditionals-lec.js
@@ -0,0 +1,103 @@
+// var raining = true;
+//
+// if (raining) {
+//     alert("Don't forget your umbrella today!");
+// }
+//
+// var lives = 0;
+//
+// if (lives === 0) {
+//     alert("GAME OVER");
+// }
+//
+//
+// var weShouldDeleteStuff = confirm('Are you sure you want to delete everything?');
+//
+// if (weShouldDeleteStuff) {
+//     // delete all the things...
+// } else {
+//     alert("Operation Canceled!");
+// }
+//
+//
+// var apple = true;
+//
+// if (apple) {
+//     alert("You have an iPhone");
+// } else {
+//     alert("You have an android");
+// }
+//
+//
+// var pizzaPreference = prompt("What kind of pizza do you like?");
+//
+// if (pizzaPreference === "pineapple and hot sauce") {
+//     alert("What a coincidence, that's my favorite!");
+// } else if (pizzaPreference === "cheese") {
+//     alert("Just plain cheese? Okay...");
+// } else {
+//     alert(pizzaPreference + " isn't my favorite, but let's order some!");
+// }
+//
+// var phone = prompt("What OS is your phone?");
+// if (phone === "apple") {
+//     alert("You have an iPhone");
+// } else if (phone === "android") {
+//     alert("You have an android");
+// } else {
+//     alert("What is that?");
+// }
+// var pizzaPreference = prompt("What kind of pizza do you like?");
+//
+// switch(pizzaPreference) {
+//     case "pineapple and hot sauce":
+//         alert("What a coincidence, that's my favorite!");
+//         break;
+//     case "cheese":
+//         alert("Just plain cheese? Okay...");
+//         break;
+//     default:
+//         alert(pizzaPreference + " isn't my favorite, but let's order some!");
+//         break;
+// }
+
+
+// function evaluateTrafficLight(lightColor) {
+//     if (lightColor === "green") {
+//         return ("You're good to go!");
+//     } else if (lightColor === "yellow") {
+//         return ("Be cautious");
+//     } else {
+//         return ("You might want to stop");
+//     }
+// }
+// var evaluateTrafficLight = prompt("What color is the traffic light?");
+// switch(evaluateTrafficLight) {
+// case "green":
+//     alert("You're Good To Go!");
+//     break;
+// case "yellow":
+//     alert("Be Cautious");
+//     break;
+// default:
+//     alert("You might want to stop");
+// }
+//
+// console.log(evaluateTrafficLight);
+
+
+// 'use strict';
+
+// var message;
+// var success = false;
+//
+// if (success) {
+//     message = "Operation was successful.";
+// } else {
+//     message = "Oops, something went wrong.";
+// }
+
+// the above if/else can be re-written as:
+// var message = (success) ? "Operation was successful." : "Oops, something went wrong.";
+
+
