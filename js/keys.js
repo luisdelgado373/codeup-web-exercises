f6810881ab2ea80fb3f5656aec4e65405cddc420
Luis Delgado <delgadoluis0510@gmail.com>
2019-09-10 15:14:51 -0500

    updated keys

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,3 @@
 var mapboxToken = 'pk.eyJ1IjoibGRlbGdhZG8zNzMiLCJhIjoiY2p6aWhhZ3phMTZzaDNrcHAxenNhdDdiOCJ9.ErUg42_hXkQWxhWXeB_vxQ';
 var darkSkyKey = '9c0de7a0b32dacc7266ae76efd997c64';
+var gitHubToken = '401005f4b32996665715e5255e7d26160b0e3774';
